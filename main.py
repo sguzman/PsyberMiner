9c520a05deaf41a7943b28ac6a9141fee3cce773
Salvador Guzman <guzmansalv@gmail.com>
2017-11-20 06:12:52 -0800

    scrap hidden vars

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import argparse
 import sys
 import requests
 import bs4
+import urllib3
 
 parser = argparse.ArgumentParser(description='Pass your UCSB GOLD login information')
 
@@ -13,4 +14,5 @@ args = parser.parse_args(sys.argv[1:])
 
 r = requests.get('https://my.sa.ucsb.edu/gold/login.aspx')
 soup = bs4.BeautifulSoup(r.text, 'html.parser')
-print(soup.prettify())
+hidden = [[x['name'], x['value']] for x in soup.find_all('input') if x['type'] == 'hidden']
+null = [print(x) for x in hidden]
