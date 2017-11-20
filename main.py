b156bafed3bf56abc8da9ec21b47a8c1f3dff215
Salvador Guzman <guzmansalv@gmail.com>
2017-11-20 05:44:59 -0800

    add abbreviations for cmd args

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import sys
 
 parser = argparse.ArgumentParser(description='Pass your UCSB GOLD login information')
 
-parser.add_argument('--user')
-parser.add_argument('--pass')
-parser.add_argument('--old', action='store_true')
+parser.add_argument('-u', '--user')
+parser.add_argument('-p', '--pass')
+parser.add_argument('-o', '--old', action='store_true')
 
 args = parser.parse_args(sys.argv[1:])
 print(args)
