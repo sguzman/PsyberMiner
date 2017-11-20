152fc4eca4fd7d2fa0262e4923f64990af599d31
Salvador Guzman <guzmansalv@gmail.com>
2017-11-20 07:36:05 -0800

    logged in successfully

--- main.py
+++ main.py
@@ -11,18 +11,22 @@ parser.add_argument('-o', '--old', action='store_true')
 
 args = parser.parse_args(sys.argv[1:])
 
-r = requests.get('https://my.sa.ucsb.edu/gold/login.aspx')
-soup = bs4.BeautifulSoup(r.text, 'html.parser')
-hidden = [[x['name'], x['value']] for x in soup.find_all('input') if x['type'] == 'hidden']
-null = [print(x) for x in hidden]
+r1 = requests.get('https://my.sa.ucsb.edu/gold/login.aspx')
+soup = bs4.BeautifulSoup(r1.text, 'html.parser')
 
-hiddenBody = '&'.join(x[0] + '=' + requests.utils.quote(x[1], safe='') for x in hidden)
-creds = [
-    'ctl00%24pageContent%24PermPinLogin%24userNameText=' + args.user,
-    'ctl00%24pageContent%24PermPinLogin%24passwordText=' + args.password,
-    'ctl00%24pageContent%24PermPinLogin%24loginButton.x=0'
-    'ctl00%24pageContent%24PermPinLogin%24loginButton.y=0'
+hidden = [[x['name'], x['value']] for x in soup.find_all('input') if x['type'] == 'hidden'] + [
+    ['ctl00%24pageContent%24PermPinLogin%24userNameText', args.user],
+    ['ctl00%24pageContent%24PermPinLogin%24passwordText', args.password],
+    ['ctl00%24pageContent%24PermPinLogin%24loginButton.x', '0'],
+    ['ctl00%24pageContent%24PermPinLogin%24loginButton.y', '0']
 ]
-credsBody = '&'.join(creds)
-body = hiddenBody + credsBody
+
+preBody = [x[0] + '=' + requests.utils.quote(x[1], safe='') for x in hidden]
+body = '&'.join(preBody)
 print(body)
+
+head = {'Content-Type': 'application/x-www-form-urlencoded'}
+r2 = requests.post('https://my.sa.ucsb.edu/gold/login.aspx', headers=head, data=body, cookies=r1.cookies)
+
+print(r2.status_code)
+print(r2.text)
