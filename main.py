708503236d9cc5c810389a564c0f8d38f0d1e027
Salvador Guzman <guzmansalv@gmail.com>
2017-11-20 06:36:36 -0800

    assembled body to send in post

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ import argparse
 import sys
 import requests
 import bs4
-import urllib3
 
 parser = argparse.ArgumentParser(description='Pass your UCSB GOLD login information')
 
 parser.add_argument('-u', '--user')
-parser.add_argument('-p', '--pass')
+parser.add_argument('-p', '--password')
 parser.add_argument('-o', '--old', action='store_true')
 
 args = parser.parse_args(sys.argv[1:])
@@ -16,3 +15,14 @@ r = requests.get('https://my.sa.ucsb.edu/gold/login.aspx')
 soup = bs4.BeautifulSoup(r.text, 'html.parser')
 hidden = [[x['name'], x['value']] for x in soup.find_all('input') if x['type'] == 'hidden']
 null = [print(x) for x in hidden]
+
+hiddenBody = '&'.join(x[0] + '=' + requests.utils.quote(x[1], safe='') for x in hidden)
+creds = [
+    'ctl00%24pageContent%24PermPinLogin%24userNameText=' + args.user,
+    'ctl00%24pageContent%24PermPinLogin%24passwordText=' + args.password,
+    'ctl00%24pageContent%24PermPinLogin%24loginButton.x=0'
+    'ctl00%24pageContent%24PermPinLogin%24loginButton.y=0'
+]
+credsBody = '&'.join(creds)
+body = hiddenBody + credsBody
+print(body)
